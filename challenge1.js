08130a2aa21995af40b98ae2a574990ed6ed71be
“Saki <sakisandra.c@gmail.com>
2023-03-02 13:59:50 -0700

    Change answers into functions

--- challenge1.js
+++ challenge1.js
@@ -22,9 +22,13 @@ fourLetters(names);
 // }
 
 var animals = ['BaT', 'COw', 'mOnKey']
-for (var i=0; i < animals.length; i++){
-    console.log(animals[i].toLowerCase());
+function lowerCased(theArray){
+    for (var i=0; i < theArray.length; i++){
+    console.log(theArray[i].toLowerCase());
+    }
 }
+
+lowerCased(animals);
 
 // 3. Given an array of strings, return only the words that begin with the letter "t".
 
@@ -34,11 +38,17 @@ for (var i=0; i < animals.length; i++){
 //     print words
 // }
 var verbs = ['running', 'walking', 'talk', 'eat']
-for (var i=0; i < verbs.length; i++){
-    if (verbs[i].endsWith('ing')){
-        console.log(verbs[i]);
+
+function ingWords(anArray){
+    for (var i=0; i < anArray.length; i++){
+        if (anArray[i].endsWith('ing')){
+            console.log(anArray[i]);
+        }
     }
 }
+
+ingWords(verbs);
+
 // 5. Start with an array of travel destinations. Print every travel destination in alphabetical order embedded in a sentence using string interpolation. For example, if the destination is "New York City", print something like "The next place I want to visit is New York City!" 
 
 // 6. Given a sentence with only lowercase letters, print the same sentence with the first letter of every word capitalized. For example, if you were given "Turing is the best", return "Turing Is The Best" instead!
